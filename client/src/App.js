6a0805fcb9eaa5907052b671532a07f8c210c1af
k1te <kmiller343@gmail.com>
2020-11-18 15:38:55 -0600

    added directory route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavTabs from "./components/NavTabs"
+import Navbar from "./components/Navbar"
 import Home from "./components/pages/Home"
+import Directory from "./components/pages/Directory"
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,8 +12,9 @@ function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <Navbar />
         <Route exact path="/" component={Home} />
+        <Route exact path="/directory" component={Directory} />
       </div>
     </Router>
   );
