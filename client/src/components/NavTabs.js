7f2db74a545658502c86365dd70c64d556df427f
k1te <kmiller343@gmail.com>
2020-11-18 15:05:10 -0600

    created basic navtab component

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const NavTabs = () => {
+    const location = useLocation();
+
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/" className={location.pathname === "/directory" ? "nav-link active" : "nav-link"}>Employee Directory</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavTabs;
