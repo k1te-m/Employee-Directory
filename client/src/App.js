cae7a563bc5e5c4d3fab95bf772335d2a3de6c79
k1te <kmiller343@gmail.com>
2020-11-18 15:05:22 -0600

    imported navtabs and home, added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavTabs from "./components/NavTabs"
+import Home from "./components/pages/Home"
 
 import logo from './logo.svg';
 import './App.css';
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
+        <Route exact path="/" component={Home} />
       </div>
     </Router>
   );
