e2e2d7f2eaa5af76567b4140eed5039af3b48ee8
k1te <kmiller343@gmail.com>
2020-11-18 19:56:03 -0600

    added file back

---
+++ client/src/components/pages/Directory.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Table from "../Table";
+
+function Directory() {
+    return (
+        <div className="container">
+            <Table />
+        </div>
+    )
+}
+
+export default Directory;
