8868abfb46393921d3cc358091c34e8ffae781f7
k1te <kmiller343@gmail.com>
2020-11-18 18:35:18 -0600

    formatted doc

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar"
-import Home from "./components/pages/Home"
-import Directory from "./components/pages/Directory"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Directory from "./components/pages/Directory";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/directory" component={Directory} />
-     
+      <Navbar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/directory" component={Directory}/>
     </Router>
   );
 }
