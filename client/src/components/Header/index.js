bde7ddaf4c5d2afb4edd4981298b7a1258222a42
k1te <kmiller343@gmail.com>
2020-11-19 17:55:30 -0600

    wrote basic header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return(
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <h4>Welcome to the Widget Company Employee Directory!</h4>
+        </div>
+    )
+
+}
+
+export default Header;
