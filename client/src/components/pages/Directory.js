a9474256ad22198adf12d8c9c8f9d2d6ad5a43d8
k1te <kmiller343@gmail.com>
2020-11-18 20:06:40 -0600

    removed duplicate container from directory comp

--- client/src/components/pages/Directory.js
+++ client/src/components/pages/Directory.js
@@ -2,11 +2,7 @@ import React from "react";
 import Table from "../Table";
 
 function Directory() {
-    return (
-        <div className="container">
-            <Table />
-        </div>
-    )
+  return <Table />;
 }
 
 export default Directory;
