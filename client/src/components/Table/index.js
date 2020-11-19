3e58a40fc2c822e7e07d4f47c424d521a71dd693
k1te <kmiller343@gmail.com>
2020-11-18 19:42:04 -0600

    updated logic for how TableData is rendered

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
-import employees from "../../employees.json";
+import employeeList from "../../employees.json";
 import TableData from "../TableData";
 
 function Table() {
   const [tableState, setTableState] = useState({
-    employees: employees,
+    employees: employeeList,
   });
-  
+
+  const employees = tableState.employees.results;
+
+  const tableDatas = employees.map((employee) => {
+    return <TableData key={employee.id.value} value={employee} />;
+  });
   return (
-    <table>
-      <thead>
-        <th scope="col">Photo</th>
-        <th scope="col">Name</th>
-        <th scope="col">Country</th>
-        <th scope="col">Email</th>
-        <th scope="col">Phone Number</th>
-        <th scope="col">Date of Birth</th>
-      </thead>
-      <tbody>
-          <TableData data={tableState.employees}  />
-      </tbody>
-    </table>
+    <div className="container">
+      <table>
+        <thead>
+          <th scope="col">Photo</th>
+          <th scope="col">Name</th>
+          <th scope="col">Country</th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">Date of Birth</th>
+        </thead>
+        <tbody>{tableDatas}</tbody>
+      </table>
+    </div>
   );
 }
 
